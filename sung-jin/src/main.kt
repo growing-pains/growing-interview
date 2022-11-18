fb4a1330ababfb18b469493e3713736d87b55833
Sung-jin <osj4872@gmail.com>
2022-11-18 21:45:36 +0900

    BOJ9012: 스택

--- sung-jin/src/main.kt
+++ sung-jin/src/main.kt
@@ -1,23 +1,34 @@
 import algorithm.BOJ10828
+import src.algorithm.BOJ9012
 
 fun main(args: Array<String>) {
-    val problem = BOJ10828()
+//    val problem = BOJ10828()
+//    problem.solution("""
+//        14
+//        push 1
+//        push 2
+//        top
+//        size
+//        empty
+//        pop
+//        pop
+//        pop
+//        size
+//        empty
+//        pop
+//        push 3
+//        empty
+//        top
+//    """.trimIndent())
 
+    val problem = BOJ9012()
     problem.solution("""
-        14
-        push 1
-        push 2
-        top
-        size
-        empty
-        pop
-        pop
-        pop
-        size
-        empty
-        pop
-        push 3
-        empty
-        top
+        6
+        (())())
+        (((()())()
+        (()())((()))
+        ((()()(()))(((())))()
+        ()()()()(()()())()
+        (()((())()(
     """.trimIndent())
 }
