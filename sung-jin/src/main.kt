0a89b4510c7233d7166b5b43b1be0ceb1a408e5c
Sung-jin <osj4872@gmail.com>
2022-11-21 23:18:12 +0900

    stack: BOJ1406, BOJ10799

--- sung-jin/src/main.kt
+++ sung-jin/src/main.kt
@@ -1,4 +1,6 @@
 import algorithm.BOJ10828
+import src.algorithm.BOJ10799
+import src.algorithm.BOJ1406
 import src.algorithm.BOJ9012
 
 fun main(args: Array<String>) {
@@ -21,14 +23,55 @@ fun main(args: Array<String>) {
 //        top
 //    """.trimIndent())
 
-    val problem = BOJ9012()
+//    val problem = BOJ9012()
+//    problem.solution("""
+//        6
+//        (())())
+//        (((()())()
+//        (()())((()))
+//        ((()()(()))(((())))()
+//        ()()()()(()()())()
+//        (()((())()(
+//    """.trimIndent())
+
+//    val problem = BOJ10799()
+//    problem.solution("()(((()())(())()))(())")
+//    problem.solution("(((()(()()))(())()))(()())")
+
+    val problem = BOJ1406()
     problem.solution("""
-        6
-        (())())
-        (((()())()
-        (()())((()))
-        ((()()(()))(((())))()
-        ()()()()(()()())()
-        (()((())()(
+        abcd
+        3
+        P x
+        L
+        P y
+    """.trimIndent())
+    problem.solution("""
+        abc
+        9
+        L
+        L
+        L
+        L
+        L
+        P x
+        L
+        B
+        P y
+    """.trimIndent())
+    problem.solution("""
+        dmih
+        11
+        B
+        B
+        P x
+        L
+        B
+        B
+        B
+        P y
+        D
+        D
+        P z
     """.trimIndent())
 }
